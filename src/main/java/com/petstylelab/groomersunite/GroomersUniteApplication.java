95ce71d9ebd6f7d8ef34463e87d5cb83b16c2ab0
최두영 <71247713+DuYeong0020@users.noreply.github.com>
2024-11-28 17:30:59 +0900

    first commit

---
+++ src/main/java/com/petstylelab/groomersunite/GroomersUniteApplication.java
@@ -0,0 +1,13 @@
+package com.petstylelab.groomersunite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomersUniteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroomersUniteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petstylelab/groomersunite/GroomersUniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petstylelab.groomersunite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomersUniteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
