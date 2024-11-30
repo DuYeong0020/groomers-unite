ab69588873325bd22dfce2fca6da66618be8f580
최두영 <71247713+DuYeong0020@users.noreply.github.com>
2024-11-30 19:22:45 +0900

    feat(user): User 엔티티에 이메일 필드 추가
    - User 엔티티에 `email` 필드 추가

--- src/main/java/com/petstylelab/groomersunite/domain/user/User.java
+++ src/main/java/com/petstylelab/groomersunite/domain/user/User.java
@@ -13,6 +13,7 @@ public class User extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String login_id;
+    private String email;
     private String password;
     private String nickname;
     @Enumerated(EnumType.STRING)
