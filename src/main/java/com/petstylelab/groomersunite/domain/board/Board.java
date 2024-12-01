6d09fa3c88fca4cb58748040cbbf4ce157b42946
최두영 <71247713+DuYeong0020@users.noreply.github.com>
2024-12-01 22:30:14 +0900

    fix: Board와 User의 연관관계를 N:1로 수정
    - 기존 Board와 User의 연관관계가 1:N으로 잘못 설정되어 있던 부분을 수정

--- src/main/java/com/petstylelab/groomersunite/domain/board/Board.java
+++ src/main/java/com/petstylelab/groomersunite/domain/board/Board.java
@@ -2,6 +2,7 @@ package com.petstylelab.groomersunite.domain.board;
 
 import com.petstylelab.groomersunite.domain.BaseEntity;
 import com.petstylelab.groomersunite.domain.comment.Comment;
+import com.petstylelab.groomersunite.domain.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -12,13 +13,18 @@ import java.util.List;
 @Entity
 public class Board extends BaseEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
 
     @Lob
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BoardImage> images = new ArrayList<>();
--- src/main/java/com/petstylelab/groomersunite/domain/user/User.java
+++ src/main/java/com/petstylelab/groomersunite/domain/user/User.java
@@ -1,16 +1,15 @@
 package com.petstylelab.groomersunite.domain.user;
 
 import com.petstylelab.groomersunite.domain.BaseEntity;
-import com.petstylelab.groomersunite.domain.board.Board;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Entity
 public class User extends BaseEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String loginId;
     private String email;
@@ -18,9 +17,5 @@ public class User extends BaseEntity {
     private String nickname;
     @Enumerated(EnumType.STRING)
     private Role role;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "board_id")
-    private Board board;
 
 }
