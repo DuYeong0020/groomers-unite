9d443d072e2e8b5a0251b67fae582bf8c13018bb
최두영 <71247713+DuYeong0020@users.noreply.github.com>
2024-12-09 21:57:11 +0900

    feat: BaseEntity에 PrePersist와 PreUpdate 메서드 추가

--- src/main/java/com/petstylelab/groomersunite/domain/BaseEntity.java
+++ src/main/java/com/petstylelab/groomersunite/domain/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.petstylelab.groomersunite.domain;
 
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -13,4 +15,16 @@ public abstract class BaseEntity {
     private LocalDateTime updatedAt;
     private String createdBy;
     private String updatedBy;
+
+
+    @PrePersist
+    void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
