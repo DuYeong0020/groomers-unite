d4f0deb9d33e94f27219a82b86e70c56228294d0
최두영 <71247713+DuYeong0020@users.noreply.github.com>
2024-12-11 21:03:37 +0900

    feat: 사용자 등록 엔드포인트를 위한 SecurityFilterChain 구성

--- src/main/java/com/petstylelab/groomersunite/config/WebSecurityConfig.java
+++ src/main/java/com/petstylelab/groomersunite/config/WebSecurityConfig.java
@@ -2,8 +2,11 @@ package com.petstylelab.groomersunite.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
@@ -12,4 +15,14 @@ public class WebSecurityConfig {
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(Customizer.withDefaults())
+                .build();
+    }
+
 }
