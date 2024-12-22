e888b2d56c7cb3688f81ea28c6c8ab8d4c4dcac6
최두영 <71247713+DuYeong0020@users.noreply.github.com>
2024-12-22 16:55:13 +0900

    feat: 회원가입 인증코드 유효성 검사 및 토큰 생성 로직 추가

--- src/main/java/com/petstylelab/groomersunite/domain/authentication/EmailVerificationTokenServiceImpl.java
+++ src/main/java/com/petstylelab/groomersunite/domain/authentication/EmailVerificationTokenServiceImpl.java
@@ -8,9 +8,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class EmailVerificationTokenServiceImpl implements EmailVerificationTokenService {
 
+    private final EmailVerificationTokenValidator emailVerificationTokenValidator;
+    private final EmailVerificationTokenFactory emailVerificationTokenFactory;
+
     @Override
     @Transactional
     public EmailVerificationTokenInfo sendRegistrationVerificationEmail(String email) {
+        emailVerificationTokenValidator.checkSendRegistrationVerificationEmail(email);
+        TokenType tokenType = TokenType.EMAIL_VERIFICATION;
+        EmailVerificationToken initVerificationToken = emailVerificationTokenFactory.createVerificationToken(email, tokenType);
         return null;
     }
 }
