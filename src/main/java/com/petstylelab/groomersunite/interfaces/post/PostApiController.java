723cbc7e07da8cb0fb427527b9d15094d27cf766
최두영 <71247713+DuYeong0020@users.noreply.github.com>
2025-01-05 17:09:07 +0900

    feat: 게시글 작성 API 추가

---
+++ src/main/java/com/petstylelab/groomersunite/interfaces/post/PostApiController.java
@@ -0,0 +1,26 @@
+package com.petstylelab.groomersunite.interfaces.post;
+
+
+import com.petstylelab.groomersunite.common.response.CommonResponse;
+import com.petstylelab.groomersunite.domain.post.PostCommand;
+import com.petstylelab.groomersunite.domain.post.PostInfo;
+import com.petstylelab.groomersunite.domain.post.PostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(("/posts"))
+@RequiredArgsConstructor
+public class PostApiController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public CommonResponse<PostDto.CreatePostResponse> createPost(@ModelAttribute @Valid PostDto.CreatePostRequest request) {
+        PostCommand.CreatePostRequest command = request.toCommand();
+        PostInfo postInfo = postService.createPost(command);
+        PostDto.CreatePostResponse response = new PostDto.CreatePostResponse(postInfo);
+        return CommonResponse.success(response);
+    }
+}
