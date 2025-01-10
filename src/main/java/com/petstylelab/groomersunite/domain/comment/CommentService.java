95ea3b1ce104960a5caa59b56fa55be79fda0233
최두영 <71247713+DuYeong0020@users.noreply.github.com>
2025-01-11 00:51:38 +0900

    feat: 단일 댓글 조회 API 추가

--- src/main/java/com/petstylelab/groomersunite/domain/comment/CommentService.java
+++ src/main/java/com/petstylelab/groomersunite/domain/comment/CommentService.java
@@ -2,7 +2,10 @@ package com.petstylelab.groomersunite.domain.comment;
 
 public interface CommentService {
 
+    CommentInfo getCommentById(Long postId, Long commentId);
+
     CommentInfo createComment(CommentCommand.CreateCommentRequest request);
 
     CommentInfo updateComment(CommentCommand.UpdateCommentRequest request);
+
 }
